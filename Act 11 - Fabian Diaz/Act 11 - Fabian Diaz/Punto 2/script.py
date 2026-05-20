b1ce86f755e5c09a9855f56dc98a8f8a920f60fc
zyrouvvv <fd813050@gmail.com>
2026-05-20 16:34:46 -0300

    Delete Act 11 - Fabian Diaz/Act 11 - Fabian Diaz directory

--- Act 11 - Fabian Diaz/Act 11 - Fabian Diaz/Punto 2/script.py
+++
@@ -1,8 +0,0 @@
-#2. Calcular el sueldo mensual de un operario conociendo la cantidad de horas trabajadas y el valor por hora.
-
-cHoras = int(input("Ingrese la cantidad de horas trabajadas: "))
-vHora = int(input("Ingrese el valor por hora: "))
-
-sueldo = cHoras * vHora
-
-print(f"El sueldo mensual es de {sueldo}")
