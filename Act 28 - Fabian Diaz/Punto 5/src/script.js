ffd77a15ddf1ee113249d351a4259757bad85041
zyrouvvv <fd813050@gmail.com>
2026-09-08 15:37:49 -0300

    Delete Act 28 - Fabian Diaz directory

--- Act 28 - Fabian Diaz/Punto 5/src/script.js
+++
@@ -1,6 +0,0 @@
-/*5. Confeccionar una página que muestre un mensaje con la función alert inmediatamente
-después que se cargue.*/
-
-function mostrarMensaje(){
-        alert("Bienvenido").focus()
-}
